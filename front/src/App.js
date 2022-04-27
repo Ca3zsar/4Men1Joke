0dcc76787aed9b58ee71aad8a94c6f37e0d5080b
Cezar Todirisca <cezartodirisca@gmail.com>
2022-04-27 14:46:52 +0300

    test CI/CD

--- front/src/App.js
+++ front/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ciuciu
+          Learn React
         </a>
       </header>
     </div>
