1d4ab3d1a22c43e24f097142d93392fe99d2c204
Cezar Todirisca <cezartodirisca@gmail.com>
2022-04-27 15:56:36 +0300

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React TE ROG IO FRUMOS AAAAAAAAAAAx
+          Carnaciori
         </a>
       </header>
     </div>
