15f90f1f2fb236d124fb29c4d62d9d907646344e
Burca Rafael <39907750+Cr34TIVv3@users.noreply.github.com>
2022-05-02 16:46:44 +0300

    getting firebase creds from secret manager

--- API/mysite/settings.py
+++ API/mysite/settings.py
@@ -6,6 +6,8 @@ from urllib.parse import urlparse
 import environ
 from google.cloud import secretmanager
 
+from . import firebase
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -15,6 +17,7 @@ env_file = os.path.join(BASE_DIR, ".env")
 
 if os.path.isfile(env_file):
     # Use a local secret file, if provided
+    firebase.initialize_firebase()
 
     env.read_env(env_file)
 elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
@@ -25,8 +28,13 @@ elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
-
     env.read_env(io.StringIO(payload))
+
+    # Initialize Firebase
+    settings_name = os.environ.get("SETTINGS_NAME", "api-firestore-creds")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+    firebase.initialize_firebase(payload)
 else:
     raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 # [END gaestd_py_django_secret_config]
@@ -138,5 +146,4 @@ STATICFILES_DIRS = []
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-from . import firebase
-firebase.initialize_firebase()
+
