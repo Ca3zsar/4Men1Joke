f11318824599eadb034ebfa89bba21a368883278
Cezar Todirisca <cezartodirisca@gmail.com>
2022-05-21 12:38:49 +0300

    Added verifications
    Co-Authored-By: Iustin Petrariu <57269343+IustinianPetrariu@users.noreply.github.com>

--- Client/src/app/post/post.component.ts
+++ Client/src/app/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, NgModule, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 @Component({
   selector: 'app-post',
@@ -9,9 +10,16 @@ export class PostComponent implements OnInit {
   selectedTags :string[] = [];
   listOfTags = ["not_funny", "funny", "not_important", "important", "nsfw"];
   selectedOption : any;
+  post_form :FormGroup;
+  validationError : boolean = false;
+
   @ViewChild('imageUploader') imageUploader : FileUploadComponent | undefined; 
 
-  constructor() { 
+  constructor(fb: FormBuilder) { 
+    this.post_form = fb.group({
+      "title": [null, Validators.compose([Validators.required])],
+      "content": [null, Validators.compose([Validators.required])]
+    });
   }
 
   addNewTag() {
@@ -21,6 +29,30 @@ export class PostComponent implements OnInit {
     var yourSelect = document.getElementById( "selector" ) as HTMLSelectElement;
     yourSelect.selectedIndex = 0;
   }
+
+  submitPost(){
+    this.markFormTouched(this.post_form);
+    if (this.post_form.valid && this.selectedTags.length > 0) {
+      var formValues = this.post_form.getRawValue();
+      console.log(formValues);
+    }else{
+      if(this.selectedTags.length == 0)
+      {
+        this.validationError = true;
+      }else{
+        this.validationError = false;
+      }
+      console.log("cacat");
+    }
+  }
+
+  markFormTouched(group: FormGroup | FormArray) {
+    Object.keys(group.controls).forEach((key: string) => {
+      const control = (group.controls as any)[key ];
+      if (control instanceof FormGroup || control instanceof FormArray) { control.markAsTouched(); this.markFormTouched(control); }
+      else { control.markAsTouched(); };
+    });
+  };
 
   ngOnInit(): void {
     this.selectedOption = "Choose a tag";
