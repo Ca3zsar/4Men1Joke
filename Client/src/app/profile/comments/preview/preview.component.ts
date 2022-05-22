e1785db893221b2609a15f649b9c2d4061e0ce1d
Burca Rafael <39907750+Cr34TIVv3@users.noreply.github.com>
2022-05-22 22:17:00 +0300

    horia brenciu strikes again

---
+++ Client/src/app/profile/comments/preview/preview.component.ts
@@ -0,0 +1,110 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
+import { UserService } from 'src/app/_services/user.service';
+
+@Component({
+  selector: 'app-preview',
+  templateUrl: './preview.component.html',
+  styleUrls: ['./preview.component.css']
+})
+export class PreviewComponent implements OnInit {
+  @Input() jsonString?: any;
+
+  userService: UserService;
+  tokenStorage: TokenStorageService;
+
+  constructor(userService: UserService, tokenStorage: TokenStorageService) {
+    this.userService = userService;
+    this.tokenStorage = tokenStorage;
+  }
+
+  id = '';
+  joke_key = '';
+
+  author = '';
+  createdAt = '';
+  content = '';
+  photo_url = '';
+  catOk_count = '';
+  BASADO_count = '';
+  questionmark_count = '';
+  keys?: string[];
+
+
+  toggledOn_catOk = false;
+  toggledOn_BASSADO = false;
+  toggledOn_questionmark = false;
+
+  ngOnInit(): void {
+    if(this.jsonString) {
+      
+      console.log(this.jsonString);
+
+      this.id = this.jsonString.id;
+      this.joke_key = this.jsonString["joke_key"];
+
+      this.author = this.jsonString.joke["author"];
+      this.createdAt = this.jsonString.joke["createdAt"];
+      this.content = this.jsonString.joke["content"];
+      this.photo_url = this.jsonString.joke["photo_url"];
+
+      this.catOk_count = this.jsonString.joke["catOk_count"];
+      this.BASADO_count = this.jsonString.joke["BASADO_count"];
+      this.questionmark_count = this.jsonString.joke["questionmark_count"];
+      
+    }
+  }
+
+  changeButtonState(reaction: string, toggledOn: boolean) {
+    if(toggledOn) {
+      document.getElementById(reaction + `_${this.joke_key}`)?.classList.add("border", "border-info", "rounded-pill", "mr-2", "ml-2");
+    }
+    else {
+      document.getElementById(reaction + `_${this.joke_key}`)?.classList.remove("border", "border-info", "rounded-pill", "mr-2", "ml-2");
+
+    }
+  }
+
+  countUpDown(reaction: string) {
+
+    if(Object.keys(this.tokenStorage.getUser()).length === 0) return;
+
+    if(reaction == "catOk") {
+      if (this.toggledOn_catOk) {
+        this.catOk_count = (parseInt(this.catOk_count) - 1).toString()
+        this.userService.catOk_countdown(this.joke_key).subscribe();
+      }
+      else {
+        this.catOk_count = (parseInt(this.catOk_count) + 1).toString();
+        this.userService.catOk_countup(this.joke_key).subscribe();
+      }
+      this.toggledOn_catOk = !this.toggledOn_catOk;
+      this.changeButtonState(reaction, this.toggledOn_catOk);
+    } 
+    else if(reaction == "BASADO") {
+      if (this.toggledOn_BASSADO) {
+        this.BASADO_count = (parseInt(this.BASADO_count) - 1).toString();
+        this.userService.BASADO_countdown(this.joke_key).subscribe();
+      }
+      else {
+        this.BASADO_count = (parseInt(this.BASADO_count) + 1).toString();
+        this.userService.BASADO_countup(this.joke_key).subscribe();
+      }
+      this.toggledOn_BASSADO = !this.toggledOn_BASSADO;
+      this.changeButtonState(reaction, this.toggledOn_BASSADO);
+    } 
+    else if(reaction == "questionmark") {
+      if (this.toggledOn_questionmark) {
+        this.questionmark_count = (parseInt(this.questionmark_count) - 1).toString();
+        this.userService.questionmark_countdown(this.joke_key).subscribe();
+      }
+      else {
+        this.questionmark_count = (parseInt(this.questionmark_count) + 1).toString();
+        this.userService.questionmark_countup(this.joke_key).subscribe();
+      }
+      this.toggledOn_questionmark = !this.toggledOn_questionmark;
+      this.changeButtonState(reaction, this.toggledOn_questionmark);
+    }
+  }
+
+}
