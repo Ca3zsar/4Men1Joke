58a7e57193d64159c8b3cbc25f268bedcf88d781
Cezar Todirisca <cezartodirisca@gmail.com>
2022-05-22 03:58:38 +0300

    updated UI + added filters UI

--- Client/src/app/home/filter/filter.component.ts
+++ Client/src/app/home/filter/filter.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import {MatCheckboxModule} from '@angular/material/checkbox'; 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
+  tags = [
+    "funny", "very funny", "serious", "cringe"
+  ];
+  
+  selectedItems = [
+
+  ];
+  dropdownSettings:IDropdownSettings = {
+    singleSelection: false,
+    idField: 'item_id',
+    textField: 'item_text',
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    itemsShowLimit: 3,
+  };
 
   constructor() { }
 
